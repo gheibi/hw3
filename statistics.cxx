d2141be5ab3e243edd8783ccd1065bd1d91ece66
Ali Gheibi <ali@linux-1ld5.site>
2015-11-15 21:42:16 +0100

    Finally after exhausted effort I did it.

--- statistics.cxx
+++ statistics.cxx
@@ -1,19 +1,42 @@
 #include <cmath>
 #include <iostream>
 #include <cstdlib>
-
+//------------Begin the added part 1---------------------------
+#include <ctime>
+//---------------End 1------------------------
 using namespace std;
-//--------------------------------------------------
-// plenty of lines here
-//--------------------------------------------------
+
+//---------------Begin 2-----------------------------------------
+double Nmean,Nvar;
+  void random_number(double*x, int N){
+      for (int i=0; i<N; i++)
+         x[i]= double (rand())/RAND_MAX;    
+  }
+  
+  void mean_variance (double*x, int N, double & mean,double & var){
+      for (int i=0; i>=N; i++){
+         Nmean+=x[i];
+      }
+      mean=Nmean/N;
+      
+      for (int i=0; i>=N; i++){
+	 Nvar+=pow(x[i]-mean,2);
+      } 
+      var=Nvar/N;
+  }
+//--------------End 2-----------------------------------------
 
 int main(){
    const int N = 100;
    double p[N];
    double mean, var;
 
-   // Some lines here....
-
+//--------------Begin 3-----------------------------------------
+   srand (time(NULL));
+   random_number(p,N);
+   mean_variance(p,N,mean,var);
+   for(int i=0; i<N; i++) cout  <<"random_number["<<i<<"]"<<"\t"<< p[i] << endl;
+//--------------Start 3-----------------------------------------
    cout << "Mean = " << mean << endl;
    cout << "Variance = " << var << endl;
 
